011740af51fb4613d0c2794abd923ac7e48a46f1
PavelKitaev <A359@live.ru>
2025-12-04 11:22:46 +0300

    init

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.2.3"
+    id("io.spring.dependency-management") version "1.1.4"
+}
+
+group = "com.example"
+version = "1.0"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    // Swagger/OpenAPI
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+// Явно указываем класс для bootRun
+tasks.getByName<org.springframework.boot.gradle.tasks.run.BootRun>("bootRun") {
+    mainClass.set("com.example.taskmanager.TaskManagerApplication")
+}
